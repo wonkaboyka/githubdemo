57531ade41150a0c73a6ac07689d8a288b480c7e
Vũ Trung Nguyên <vunguyenhp99@gmail.com>
2023-06-08 22:11:11 +0900

    File Trangchu.Trangchuhtml

---
+++ portfolioJIBC/portfolioJIBC/main.js
@@ -0,0 +1,30 @@
+// alert('hi Javascript basic!')
+var fullName = 'Vũ Trung Nguyên';
+var age = '29 tuổi';
+// alert(fullName);
+// alert(age);
+console.warn(fullName);
+
+confirm('Bà vợ tôi không chịu đi ngủ');
+
+prompt('Điền tuổi của bạn để xác nhận!!!');
+
+setTimeout(function() {
+    alert('Đã bảo điền thì điền sớm đi cho rồi!! Lì cái mặt')
+}, 1000);
+
+// setInterval(function() {
+//     console.log('thông báo' + Math.random())
+// }, 2000);
+
+// var toántusohoc = 1+2;
+
+// console.log(toántusohoc);
+
+// var a = 1;
+
+// var b = 2;
+
+// if (a == b) {
+//     alert('đúng')
+// };
